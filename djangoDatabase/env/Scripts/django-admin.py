c77816d068421ccedd23ab49842823075a422695
kiara <kiarawestbrooks@gmail.com>
2016-01-12 12:48:23 -0800

    Made changes to models

---
+++ djangoDatabase/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Kiara\Desktop\LAP\Python_Anywhere\djangoDatabase\djangoDatabase\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
