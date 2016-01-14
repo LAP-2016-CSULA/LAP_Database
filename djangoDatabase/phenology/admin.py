a759ff492b0df27bda8ebcb0fcaca5a435f621cd
kiara <kiarawestbrooks@gmail.com>
2016-01-14 14:56:58 -0800

    Attempt at implementing questions -> answers. However, there is an error that I need to fix before being able to proceed.

--- djangoDatabase/phenology/admin.py
+++ djangoDatabase/phenology/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from phenology.models import SpeciesType, Species
+from phenology.models import SpeciesType, Species, SpeciesAnswer, SpecificSpeciesQuestion
 
 admin.site.register(SpeciesType)
 admin.site.register(Species)
+admin.site.register(SpecificSpeciesQuestion)
+admin.site.register(SpeciesAnswer)
--- djangoDatabase/phenology/models.py
+++ djangoDatabase/phenology/models.py
@@ -20,9 +20,36 @@ class Species(models.Model):
     """
     Exact organism in a species group e.g. a great dane (type of dog)
     """
-    species_type_id = models.ForeignKey(SpeciesType, on_delete=models.CASCADE) 
-    common_name = models.CharField(max_length=45)
-    scientific_name = models.CharField(max_length=255)
+    species_type = models.ForeignKey(SpeciesType, on_delete=models.CASCADE) 
+    common_name = models.CharField(max_length=45, help_text="Enter the common known name of the species e.g. human")
+    scientific_name = models.CharField(max_length=255, help_text="Enter the scientific name of a species e.g. homo sapiens")
+    general_info = models.CharField(max_length=4096, default="",  help_text="Enter general information about the species")
 
     def __str__(self):
         return self.common_name
+
+@python_2_unicode_compatible
+class SpecificSpeciesQuestion(models.Model):
+    """
+    Questions concerning the phenological information of a specific species
+    """
+    subject_species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    question = models.CharField(max_length=255, help_text="But a question about a species here")
+
+    def __str__(self):
+        readable_string = self.subject_species, self.question
+        return readable_string
+
+@python_2_unicode_compatible 
+class SpeciesAnswer(models.Model):
+    """
+    The answer relate to a question - currently only offers boolean answers
+    """
+    question = models.ForeignKey(SpecificSpeciesQuestion, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255, default="True or False?")
+    answer_boolean = models.BooleanField(default=False)
+
+    #Below string may change if/when we offer more answer types
+    def __str__(self):
+        return str(self.answer)
+    
